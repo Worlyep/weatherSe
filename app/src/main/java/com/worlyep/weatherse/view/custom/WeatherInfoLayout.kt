87d1756cbcfc41cf7b5cf70c5c17ad6eee96663b
worlyep <worlyep@gmail.com>
2019-12-24 14:26:51 +0900

    #3 #comment 커스텀 뷰 수정

--- app/src/main/java/com/worlyep/weatherse/view/custom/WeatherInfoLayout.kt
+++ app/src/main/java/com/worlyep/weatherse/view/custom/WeatherInfoLayout.kt
@@ -1,6 +1,7 @@
 package com.worlyep.weatherse.view.custom
 
 import android.content.Context
+import android.content.res.TypedArray
 import android.text.Html
 import android.text.Spanned
 import android.util.AttributeSet
@@ -12,12 +13,42 @@ import com.worlyep.weatherse.R
 import kotlinx.android.synthetic.main.layout_weather_info.view.*
 
 class WeatherInfoLayout : LinearLayout {
-    constructor(context: Context?) : super(context) {
+    constructor(context: Context) : super(context) {
         initView()
     }
 
-    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs) {
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
         initView()
+        getAttrs(attrs)
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyle: Int) : super(context, attrs) {
+        initView()
+        getAttrs(attrs, defStyle)
+    }
+
+    private fun getAttrs(attrs: AttributeSet) {
+        val typedArray = context.obtainStyledAttributes(attrs, R.styleable.WeatherInfoLayout)
+        setTypeArray(typedArray)
+    }
+
+    private fun getAttrs(attrs: AttributeSet, defStyle: Int) {
+        val typedArray =
+            context.obtainStyledAttributes(attrs, R.styleable.WeatherInfoLayout, defStyle, 0)
+        setTypeArray(typedArray)
+    }
+
+    private fun setTypeArray(typedArray: TypedArray) {
+        val iconUrl = typedArray.getString(R.styleable.WeatherInfoLayout_weatherIcon)
+        val name = typedArray.getString(R.styleable.WeatherInfoLayout_weatherName)
+        val humid = typedArray.getFloat(R.styleable.WeatherInfoLayout_weatherHumid, 0f).toDouble()
+        val temper = typedArray.getFloat(R.styleable.WeatherInfoLayout_weatherTemper, 0f).toDouble()
+
+        Glide.with(context).load(iconUrl).into(weatherIcon)
+        weatherName.text = name
+        setWeatherTemper(temper, humid)
+
+        typedArray.recycle()
     }
 
     private fun initView() {
@@ -35,9 +66,11 @@ class WeatherInfoLayout : LinearLayout {
         Glide.with(context).load(iconUrl).into(weatherIcon)
     }
 
-    internal fun setWeatherTemper(temper: Int?, humid: Int?) {
-        val text: String = context.getString(R.string.weather_temper_humid, temper, humid)
+    internal fun setWeatherTemper(temper: Double?, humid: Double?) {
+        val text: String =
+            context.getString(R.string.weather_temper_humid, temper?.toInt(), humid?.toInt())
         val styledText: Spanned = Html.fromHtml(text, FROM_HTML_MODE_LEGACY)
+
         weatherTemp.text = styledText
     }
 }
