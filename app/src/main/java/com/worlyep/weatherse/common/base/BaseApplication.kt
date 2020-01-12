9ff05ed3eb6ca5db79121604d0689d3796955b55
worlyep <worlyep@gmail.com>
2020-01-13 01:04:31 +0900

    Koin 추가

--- app/src/main/java/com/worlyep/weatherse/common/base/BaseApplication.kt
+++ app/src/main/java/com/worlyep/weatherse/common/base/BaseApplication.kt
@@ -2,6 +2,8 @@ package com.worlyep.weatherse.common.base
 
 import android.app.Application
 import androidx.appcompat.app.AppCompatDelegate
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
 
 /**
  * 2020-01-03
@@ -11,5 +13,9 @@ class BaseApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         AppCompatDelegate.setCompatVectorFromResourcesEnabled(true)
+
+        startKoin {
+            androidContext(applicationContext)
+        }
     }
 }
