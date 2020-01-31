3b4fa1ba3188ee194a018286d8dd05e40d3b43dc
worlyep <worlyep@gmail.com>
2020-01-31 14:01:59 +0900

    MainViewModel 주석 제거

--- app/src/main/java/com/worlyep/weatherse/ui/viewModel/MainViewModel.kt
+++ app/src/main/java/com/worlyep/weatherse/ui/viewModel/MainViewModel.kt
@@ -3,10 +3,4 @@ package com.worlyep.weatherse.ui.viewModel
 import com.worlyep.weatherse.api.ApiRepository
 import com.worlyep.weatherse.common.base.BaseViewModel
 
-/**
- * 2020-01-03
- * @author worlyep
- * @note.RxJava 노트
- * ## RxJava / RxAndroid 제공하는 스케쥴러 섹션 참조
- **/
 class MainViewModel(private val model: ApiRepository) : BaseViewModel()
